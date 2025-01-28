d206a19fe5a774a506ae3830e7b3ff2b32fa525e
zoooey99 <zoeyjleeweb@gmail.com>
2025-01-28 12:16:19 -0500

    Class Tues, Jan 28.
    Finished up to Lab 2

--- static-node/static.js
+++ static-node/static.js
@@ -7,16 +7,41 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  fs.readFile('./public/home.html', (error, content)=>{
-    //We need to handle errors first
-
-    //if no errors, we can output the content
-    res.end(content);
-  });
   
+    switch(req.url){
+      case "":
+      case "/":
+        displayPage('./public/home.html', res);
+      break;
+
+      case "/about":
+        displayPage('./public/about.html', res);
+      break;
+
+      case "/contact":
+        displayPage('./public/contact.html', res);
+      break;
+
+      default:
+        displayPage('./public/404.html', res, 404);
+    }
+
 });
+
+
+const displayPage = (path, r, status = 200)=> {
+
+  r.statusCode = status;
+  r.setHeader('Content-Type', 'text/html');
+  fs.readFile( path, (error, content) =>{
+    if(error){
+      r.statusCode = 500;
+      r.end("500 - error");
+    }
+    r.end(content);
+  })
+}
+
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
