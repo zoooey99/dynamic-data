ff5bcae0735e12a1cfcaf9f7592305396761fcff
zoooey99 <zoeyjleeweb@gmail.com>
2025-03-27 12:40:27 -0400

    class mar 27

--- file-uploads/server.js
+++ file-uploads/server.js
@@ -4,6 +4,28 @@ const app = express()
 const port = process.env.port || 3000
 const multer = require('multer')
 
+//Setup bodyParser
+const bodyParser = require('body-parser')
+
+//Define our models and init database
+const {Sequelize, Model, DataTypes } = require('sequelize')
+//create sequelize instance
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'database.sqlite'
+})
+//create your first Model
+const ProfileImage = sequelize.define('ProfileImages', {
+  url: DataTypes.STRING,
+})
+//Initialize bodyparser... converts POST request objects to json
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+//sync the models to the database
+sequelize.sync()
+
+
 //initialize hanlebars
 const handlebars = require('express-handlebars')
 app.engine('handlebars', handlebars.engine())
@@ -28,15 +50,38 @@ app.get('/', (req,res) => {
     res.render('page')
 })
 
-app.post('/profile', upload.single('avatar'), function (req, res) {
+app.post('/profile', upload.single('avatar'), async (req, res) => {
     // req.file is the name of your file in the form above, here 'avatar'
     // req.body will hold the text fields, if there were any 
     console.log(req.file, req.body)
     console.log(`original name: ${req.file.originalname}`)
     console.log(`mime type: ${req.file.mimetype}`)
     res.type('text/html')
-    res.send('uploaded!!!')
+    //res.send('uploaded!!!')
+    const img = await ProfileImage.create({
+      url: req.file.filename
+    })
+    res.json(img)
   });
+
+//Addition CRUD Routes
+// Get all images
+app.get('/images', async (req,res)=>{
+  const images = await ProfileImage.findAll()
+  res.json(images)
+})
+// Get specific image Select * From 
+app.get('/image/:id', async (req,res)=>{
+  const image = await ProfileImage.findByPk(req.params.id)
+  res.json(image)
+})
+// Delete a record
+app.get('/image/delete/:id', async (req, res) => {
+  const image = await ProfileImage.findByPk(req.params.id)
+  image.destroy()
+  res.json(image)
+})
+
 
 app.use((req,res) => {
     res.type('text/html')
